3332a7ca0785606ba87012c8cdedf9870cac9fe9
nk <alexander.andreev@me.com>
2021-04-21 02:18:48 +0300

    part 3

---
+++ db/migrate/20210419213443_create_questions.rb
@@ -0,0 +1,9 @@
+class CreateQuestions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :questions do |t|
+      t.string :body, null: false
+      t.references :test, index: true, foreign_key: { to_table: :tests }
+      t.timestamps
+    end
+  end
+end
