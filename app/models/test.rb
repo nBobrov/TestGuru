056bdfeac7315002afbedda3fc9afe731b6f1312
nk <alexander.andreev@me.com>
2021-04-24 00:44:20 +0300

    part 6

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,9 +6,14 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-  class << self
-    def find_by_category(title)
-      joins(:category).where(categories: {title: title}).order(id: :desc).pluck(:title)
-    end
-  end
+  validates :title, presence: true
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, uniqueness: true
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :middle, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+
+  scope :title_array, -> { order(id: :desc).pluck(:title) }
+  scope :find_by_category, -> (title) { joins(:category).where(categories: {title: title}).title_array }
 end
