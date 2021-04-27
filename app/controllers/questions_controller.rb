7012fd7b75a3200392918689c45d51b0f36bd4c0
nk <alexander.andreev@me.com>
2021-04-28 00:05:17 +0300

    part 7 fix save  question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,7 +16,7 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    question = @test.questions.create(question_params)
+    question = @test.questions.new(question_params)
 
     if question.save
       render plain: 'Question has been created successfully'
