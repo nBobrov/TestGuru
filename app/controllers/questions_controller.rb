cf4c523a902fed6b35129be7746cf0c3efa4615d
nk <alexander.andreev@me.com>
2021-04-30 02:40:57 +0300

    part 7 fix save  question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,33 +1,41 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index create]
-  before_action :find_question, only: %i[show destroy]
+  before_action :find_test, only: %i[new create]
+  before_action :find_question, only: %i[show edit update destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index
-
-  end
-
   def show
-    render plain: "Question: #{@question.body}"
+
   end
 
   def new
+    @question = @test.questions.new
+  end
+
+  def edit
 
   end
 
   def create
-    question = @test.questions.new(question_params)
+    @question = @test.questions.new(question_params)
 
-    if question.save
-      render plain: 'Question has been created successfully'
+    if @question.save
+      redirect_to @question
     else
-      render inline: question.errors.full_messages.join('<br/>')
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
     end
   end
 
   def destroy
     @question.destroy
-    render plain: 'Question has been deleted successfully'
+    redirect_to test_path(@question.test)
   end
 
   private
