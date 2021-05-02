3d846e58be448a5c2cd13a0c5f3ff6bd8013f0b7
nk <alexander.andreev@me.com>
2021-05-02 03:28:43 +0300

    fix part8

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,6 @@
 module QuestionsHelper
   def question_header(question)
-    if question.id.nil?
+    if question.new_record?
       "Create New #{question.test.title} Question"
     else
       "Edit #{question.test.title}  Question"
