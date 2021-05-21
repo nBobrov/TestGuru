307102e1fe4a7e887a9dfd39ecc00e95b1e36c03
nk <alexander.andreev@me.com>
2021-05-21 10:55:47 +0300

    part 11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,27 @@
 class ApplicationController < ActionController::Base
-  before_action :set_return_path
+  before_action :store_location!,
+                unless: -> { devise_controller? || request.xhr? || request.fullpath == root_path },
+                if: -> { request.get? && is_navigational_format? }
+  before_action :authenticate_user!, unless: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
-  def set_return_path
-    session[:return_path] ||= request.original_fullpath unless request.original_fullpath == new_user_session_path
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def store_location!
+    store_location_for(:user, request.fullpath)
   end
 
   def after_sign_in_path_for(resource)
+    set_flash_message! :notice, :signed_in_custom, :first_name => resource.first_name, :last_name => resource.last_name
     if current_user.is_a?(Admin)
-      session[:return_path] || admin_tests_path
+      stored_location_for(resource) || admin_tests_path
     else
-      session[:return_path] || tests_path
+      stored_location_for(resource) || tests_path
     end
   end
 end
