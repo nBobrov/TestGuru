5e53920ddc5a96e5a829f97b7add7c470caf7c75
nk <alexander.andreev@me.com>
2021-07-29 10:05:36 +0300

    fix delete data

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :test
-  has_many :answers
-  has_many :gists
+  has_many :answers, dependent: :destroy
+  has_many :gists, dependent: :destroy
+  has_many :test_passages, foreign_key: 'current_question_id', dependent: :destroy
 
   validates :body, presence: true
 end
