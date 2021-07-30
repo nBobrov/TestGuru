b7d60627775d6b2806c448e78ee77632fdd611b9
nk <alexander.andreev@me.com>
2021-07-30 09:41:15 +0300

    fix part17

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :test
   has_many :answers, dependent: :destroy
   has_many :gists, dependent: :destroy
-  has_many :test_passages, foreign_key: 'current_question_id', dependent: :destroy
+  has_many :test_passages
 
   validates :body, presence: true
 end
