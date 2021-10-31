ec28b4c71654c489b64b726fd2ac251b203dd32d
nk <alexander.andreev@me.com>
2021-11-01 02:39:48 +0300

    fix badges

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -4,7 +4,10 @@ class Badge < ApplicationRecord
 
   validates :name, :img, :rule_type, presence: true
 
-  TYPES = %i[success_passing_all_tests_from_category success_passing_test_from_the_first_try success_passing_all_tests_of_specified_level].freeze
+  TYPES = %i[all_tests_from_category first_try all_tests_of_specified_level].freeze
 
-  scope :with_user, -> (user_id) { joins('LEFT JOIN user_badges ub ON ub.badge_id=badges.id').joins("LEFT JOIN users u ON (u.id = ub.user_id AND u.id = #{user_id})").distinct }
+  scope :with_user, -> (user_id) {
+    joins('LEFT JOIN user_badges ub ON ub.badge_id=badges.id')
+      .joins("LEFT JOIN users u ON (u.id = ub.user_id AND u.id = #{user_id})").distinct
+      .order(id: :asc) }
 end
--- app/services/badge_assign_service.rb
+++ app/services/badge_assign_service.rb
@@ -1,26 +1,14 @@
 class BadgeAssignService
 
-  RULES = {
-    success_passing_all_tests_from_category: BadgeRules::AllTestsFromCategorySpecification,
-    success_passing_test_from_the_first_try: BadgeRules::FirstTrySpecification,
-    success_passing_all_tests_of_specified_level: BadgeRules::AllTestsOfSpecifiedLevelSpecification
-  }.freeze
-
   def initialize(test_passage)
     @test_passage = test_passage
   end
 
   def call
     Badge.find_each do |badge|
-      rule = RULES[badge.rule_type.to_sym].new(rule_value: badge.rule_value, test_passage: @test_passage)
-      assign_badge(badge) if rule.satisfies?
+      class_name = "BadgeRules::#{badge.rule_type.camelize}Specification"
+      rule = class_name.constantize.new(rule_value: badge.rule_value, test_passage: @test_passage)
+      @test_passage.user.badges << badge if rule.satisfies?
     end
   end
-
-  private
-
-  def assign_badge(badge)
-    UserBadge.create(user_id: @test_passage.user.id, badge_id: badge.id)
-  end
-
 end
