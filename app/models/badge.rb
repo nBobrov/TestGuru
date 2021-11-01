e35ff940e2b577d455caecc15c074a70193ea39d
nk <alexander.andreev@me.com>
2021-11-01 03:34:01 +0300

    fix badges

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -7,7 +7,6 @@ class Badge < ApplicationRecord
   TYPES = %i[all_tests_from_category first_try all_tests_of_specified_level].freeze
 
   scope :with_user, -> (user_id) {
-    joins('LEFT JOIN user_badges ub ON ub.badge_id=badges.id')
-      .joins("LEFT JOIN users u ON (u.id = ub.user_id AND u.id = #{user_id})").distinct
-      .order(id: :asc) }
+    joins("LEFT JOIN user_badges ub ON (ub.badge_id=badges.id AND ub.user_id = #{user_id})").distinct
+                                                                                            .order(id: :asc) }
 end
